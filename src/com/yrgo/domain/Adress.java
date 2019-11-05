ddc4b9a074fe20ddaf3d3af856b55103088a3f32
JohanCederschiold <johan.cederschiold@gmail.com>
2019-11-05 10:50:02 +0100

    Added missing class

---
+++ src/com/yrgo/domain/Adress.java
@@ -0,0 +1,126 @@
+package com.yrgo.domain;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Adress {
+	
+	
+
+	
+	private String street;
+	private String city;
+	private String zipcode;
+	
+	
+	public Adress() {
+		super();
+	}
+	
+	
+
+
+	public Adress(String street, String city, String zipcode) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.zipcode = zipcode;
+	}
+
+
+
+
+	public String getStreet() {
+		return street;
+	}
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Adress [street=" + street + ", city=" + city + ", zipcode=" + zipcode + "]";
+	}
+
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
+		return result;
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adress other = (Adress) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		if (zipcode == null) {
+			if (other.zipcode != null)
+				return false;
+		} else if (!zipcode.equals(other.zipcode))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+		
+
+	
+	
+	
+	
+	
+
+}
